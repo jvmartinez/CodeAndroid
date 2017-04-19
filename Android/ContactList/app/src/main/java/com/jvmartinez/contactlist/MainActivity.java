56cf863c928c4b44141d21ac943e00373b2d6618
Juan Martinez <juanmartinez.cslcv@gmail.com>
2017-04-19 13:02:08 -0400

    Modificacion maquetado, llamado en MainActivity

--- Android/ContactList/app/src/main/java/com/jvmartinez/contactlist/MainActivity.java
+++ Android/ContactList/app/src/main/java/com/jvmartinez/contactlist/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
+import com.jvmartinez.contactlist.Adapter.adapterSwipe;
 import com.jvmartinez.contactlist.Adapter.listAdapterContact;
 import com.jvmartinez.contactlist.Model.contact;
 
@@ -21,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
         listAdapterContact  adapterContact = new listAdapterContact(insertContact());
         this.recyclerView.setAdapter(adapterContact);
         this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new adapterSwipe(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT));
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
     }
 
     public List<contact> insertContact(){
         List<contact> contacts = new ArrayList<>();
-        contact contact_1 = new contact("Juan","Martinez",00000);
-        contact contact_2 = new contact("Carlos","Enock",00000);
+        contact contact_1 = new contact("Juan","Martinez",123456);
+        contact contact_2 = new contact("Carlos","Enock",67890);
+        contact contact_3 = new contact("Louis","Perdomo",12345678);
         contacts.add(contact_1);
         contacts.add(contact_2);
+        contacts.add(contact_3);
         return contacts;
     }
 }
