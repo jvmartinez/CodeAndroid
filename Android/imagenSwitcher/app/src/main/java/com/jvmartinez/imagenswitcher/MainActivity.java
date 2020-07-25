f500cc7f3e8c2cc8d8dbfbc03e50b9f0acb7a761
Juan Martinez <juanmartinez.cslcv@gmail.com>
2020-07-24 22:10:03 -0500

    update project androidx

--- Android/imagenSwitcher/app/src/main/java/com/jvmartinez/imagenswitcher/MainActivity.java
+++ Android/imagenSwitcher/app/src/main/java/com/jvmartinez/imagenswitcher/MainActivity.java
@@ -1,7 +1,6 @@
 package com.jvmartinez.imagenswitcher;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -9,6 +8,8 @@ import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ViewSwitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
